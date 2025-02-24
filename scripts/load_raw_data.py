ffa83a5e1735379df36854fbd6e6632ef2ed316c
Tanmay6461 <pawar.ta@northeastern.edu>
2025-02-24 10:58:46 -0500

    loading raw data

--- scripts/load_raw_data.py
+++ scripts/load_raw_data.py
@@ -5,12 +5,21 @@ from dotenv import load_dotenv
 import pandas as pd
 from google.cloud import storage
 from io import BytesIO, StringIO
+from snowflake.snowpark import Session
+from snowflake.snowpark.types import StructType, StructField, StringType, IntegerType
+
 
 load_dotenv()
 
 fred_api=os.getenv('FRED_API')
 fred_api_url = f"https://api.stlouisfed.org/fred/series/observations?series_id=T10Y2Y&api_key={fred_api}&file_type=json&observation_start=2020-02-23&observation_end=2025-02-23"
 bucket_name = os.getenv('GCLOUD_BUCKET_NAME')
+FRED_Table = ['FredData']
+TABLE_DICT = {
+    "fred_data": {"schema": "RAW_FRED","tables":FRED_Table}
+}
+
+
 
 def upload_data_to_gcloud(url,bucket_name):
     try: 
@@ -31,10 +40,50 @@ def upload_data_to_gcloud(url,bucket_name):
 
 
 
+def load_raw_data(session, storagedir=None,schema=None,tname=None):
+    session.use_role("FRED_ROLE")
+    session.use_database("FRED_DB")
+    session.use_schema(schema)
+    location = "@external_data.FRED_RAW_STAGE/{}".format(storagedir)
+    try:
+        copy_query = f"""
+            COPY INTO {tname}
+            FROM {location}
+            FILE_FORMAT = (TYPE = 'CSV' FIELD_OPTIONALLY_ENCLOSED_BY = '"' SKIP_HEADER = 1)
+            ON_ERROR = 'CONTINUE';
+        """
+        result = session.sql(copy_query).collect()
+        print(f"Data successfully loaded into table: {tname}")
+        print(f"Copy result: {result}")
+        # # df = session.read.parquet(location)
+        
+        # # Read Parquet file with explicit schema
+        # df = session.read.parquet(location)
+        # df.copy_into_table(table_name=tname,force=True,validation_mode="RETURN_ERRORS")
+        # print(f"data stored in {tname}")
+
+    except Exception as e:
+        print(e)
+        raise
 
 
+def load_all_tables(session):
+    # _ = session.sql("ALTER WAREHOUSE FRED_WH SET WAREHOUSE_SIZE= XSMALL WAIT_FOR_COMPLETION= TRUE").collect()
+
+    for storagedir, data in TABLE_DICT.items():
+        tnames = data['tables']
+        schema = data['schema']
+        for tname in tnames:
+            print("loading{}".format(tname))
+            # print(storagedir)
+            # print(schema)
+            load_raw_data(session,storagedir=storagedir,schema=schema,tname=tname)
+
+    # _ = session.sql("ALTER WAREHOUSE FRED_WH SET WAREHOUSE_SIZE= XSMALL").collect()
 
 
-
-# if __name__ == "__main__":  
-    # upload_data_to_gcloud(fred_api_url,bucket_name)
+if __name__ == "__main__":  
+     with Session.builder.getOrCreate() as session:
+        #  print("ok")
+         load_all_tables(session)
+        # upload_data_to_gcloud(fred_api_url,bucket_name)
