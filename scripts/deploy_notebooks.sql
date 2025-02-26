277add7791bdbdaa313675e3e5fbfd80289cdddc
sathayNEU <balachandar.sa@northeastern.edu>
2025-02-26 00:44:04 -0500

    feat: deploy udf test

--- scripts/deploy_notebooks.sql
+++ scripts/deploy_notebooks.sql
@@ -1,4 +1,8 @@
--- The objective of this notebook is to deploy notebooks using the CREATE OR REPLACE NOTEBOOK IDENTIFIER command.
--- Please note that this script is parameterized with the following parameters:
---     -- env
---     -- branch
+--!jinja
+
+CREATE OR REPLACE NOTEBOOK IDENTIFIER('"FRED_DB"."{{env}}_INTEGRATIONS"."{{env}}_DB_OBJECTS"')
+    FROM '@"FRED_DB"."INTEGRATIONS"."GIT_REPO"/branches/"{{branch}}"/notebooks/'
+    QUERY_WAREHOUSE = 'FRED_WH'
+    MAIN_FILE = 'DB_OBJECTS.ipynb';
+
+ALTER NOTEBOOK "FRED_DB"."{{env}}_INTEGRATIONS"."{{env}}_DB_OBJECTS" ADD LIVE VERSION FROM LAST;
